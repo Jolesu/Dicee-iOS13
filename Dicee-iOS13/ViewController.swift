f5fa387f9541e4c9b2cfe31d126e311c6f9b4359
Joel <joellacerdaol@gmail.com>
2022-05-19 16:40:08 -0300

    variables and arrays added

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,14 +14,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var dice1: UIImageView!
     @IBOutlet weak var dice2: UIImageView!
     
+    var leftDice = 1
+    var rightDice = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //who what    value
-        dice1.image = #imageLiteral(resourceName: "DiceSix")
-        dice2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
-
+    @IBAction func rollDice(_ sender: UIButton) {
+        dice1.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDice]
+        dice2.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDice]
+        
+        leftDice = leftDice + 1
+        rightDice = rightDice - 1
+    }
+    
 }
 
